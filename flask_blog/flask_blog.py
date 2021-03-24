ade3238d108842bc7b50e486efbfd930d20b9936
Manish Verma <vermam318@gmail.com>
2021-03-24 05:39:41 +0530

    some basics of flask structure

--- flask_blog/flask_blog.py
+++ flask_blog/flask_blog.py
@@ -3,5 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return '<h1>Hello Page Testing!</h1>'
+
+
+@app.route('/about')
+def about():
+    return '<h1>About Page Testing!</h1>'
+
+
+if __name__ == '__main__':  # name = main module if script is directly run by python
+    app.run(debug=True)
