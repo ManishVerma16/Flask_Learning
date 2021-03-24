fa7fe500d0deb1659620cc82b534624c57586e87
Manish Verma <vermam318@gmail.com>
2021-03-24 08:52:26 +0530

    name update & home route added

--- flask_blog/flask_blog.py
+++ flask_blog/flask_blog.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home')  # setting multiple route for the same page
 def home():
     return '<h1>Hello Page Testing!</h1>'
 
@@ -13,5 +13,6 @@ def about():
     return '<h1>About Page Testing!</h1>'
 
 
-if __name__ == '__main__':  # name = main module if script is directly run by python
+if __name__ == '__main__':  # name = main if script is directly run by python
     app.run(debug=True)
+# if we import this module somewhere else then this name will be the name of module.
