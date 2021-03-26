f7b4f69bd9230d15e323faa0e8d65ea31e880b27
Manish Verma <vermam318@gmail.com>
2021-03-26 23:58:04 +0530

    forms import issue resolved

--- flask_blog/flask_blog_template.py
+++ flask_blog/flask_blog_template.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import defaultload
-from forms import RegistrationForm, LoginForm
+from flask_blog.forms import RegistrationForm, LoginForm # folder_name.file_name -> flask_blog.forms
 
 app = Flask(__name__)
 
