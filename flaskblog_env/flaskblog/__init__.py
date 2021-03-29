dba09a839b7611cc1dd943d99794d689f1206e51
Manish Verma <vermam318@gmail.com>
2021-03-29 23:24:11 +0530

    Bcrypt package added

--- flaskblog_env/flaskblog/__init__.py
+++ flaskblog_env/flaskblog/__init__.py
@@ -1,16 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flaskblog.forms import RegistrationForm, LoginForm # folder_name.file_name -> flask_blog.forms
-# from models import User, Post
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '08411f065bb1bc731052710b052ab510'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///mysql.db'
-
 
 db = SQLAlchemy(app)
 
+bcrypt = Bcrypt(app)
 from flaskblog import routes
