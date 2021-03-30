909a8c1da45ded5062930a75b8553c0ed3813ac1
Manish Verma <vermam318@gmail.com>
2021-03-30 14:59:02 +0530

    registration validation completed

--- flaskblog_env/flaskblog/forms.py
+++ flaskblog_env/flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -10,8 +11,25 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exist. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exist. Please choose a different one.')
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+'''
+template for validation message
+def validate_field(self, field):
+    if True:
+        raise_ValidationError('Validation Message')
+'''
