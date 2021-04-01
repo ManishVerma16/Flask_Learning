309e0308c5bbc051423f942124956129fb8e4dda
Manish Verma <vermam318@gmail.com>
2021-04-01 12:53:39 +0530

    login manager package import

--- flaskblog_env/flaskblog/__init__.py
+++ flaskblog_env/flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app.config['SECRET_KEY'] = '08411f065bb1bc731052710b052ab510'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
-
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from flaskblog import routes
-from flaskblog.forms import RegistrationForm, LoginForm # folder_name.file_name -> flask_blog.forms
+# from flaskblog.forms import RegistrationForm, LoginForm # folder_name.file_name -> flask_blog.forms
