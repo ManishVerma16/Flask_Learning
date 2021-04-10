057fe589b9bbc8b476071a20ce6290b88c11a698
Manish Verma <vermam318@gmail.com>
2021-04-10 09:26:02 +0530

    route added for viewing post and update post

--- flaskblog_env/flaskblog/routes.py
+++ flaskblog_env/flaskblog/routes.py
@@ -1,9 +1,8 @@
 import secrets, os
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, bcrypt, db
 from flaskblog.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
-
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -116,5 +115,28 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend="New Post")
 
-    return render_template('create_post.html', title='New Post', form=form)
+@app.route('/post/<int:post_id>') # using <datatype: var_name> variable in routes
+def post(post_id):
+    post = Post.query.get_or_404(post_id)  # .get_or_404 either it return page with post_id or page doesn't exist
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend="Update Post")
