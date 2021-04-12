6dd1bd7cfff988e66e31609c987c8eabe74d3e19
Manish Verma <vermam318@gmail.com>
2021-04-12 20:08:15 +0530

    reset password functions added

--- flaskblog_env/flaskblog/forms.py
+++ flaskblog_env/flaskblog/forms.py
@@ -56,6 +56,22 @@ class PostForm(FlaskForm):
     submit = SubmitField('Post')
 
 
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data)
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
+
+
 '''
 template for validation message
 def validate_field(self, field):
