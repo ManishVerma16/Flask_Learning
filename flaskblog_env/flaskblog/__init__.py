9752d210762e985b4814159145b6e600aa367fe2
Manish Verma <vermam318@gmail.com>
2021-04-13 19:31:37 +0530

    import flask email and sending email setup done

--- flaskblog_env/flaskblog/__init__.py
+++ flaskblog_env/flaskblog/__init__.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+# import sys
+
+# sys.setrecursionlimit(10**6)
 
 app = Flask(__name__)
 
@@ -14,5 +19,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'testuser@gmail.com'
+app.config['MAIL_PASSWORD'] = 'testpass'
+mail = Mail(app)
 from flaskblog import routes
 # from flaskblog.forms import RegistrationForm, LoginForm # folder_name.file_name -> flask_blog.forms
