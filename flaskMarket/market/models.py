ac30be55e7dff4f84164e11d3569ee21d409ce33
Manish Verma <vermam318@gmail.com>
2021-04-22 15:38:42 +0530

    user model added

--- flaskMarket/market/models.py
+++ flaskMarket/market/models.py
@@ -1,4 +1,17 @@
+from enum import unique
+
+from sqlalchemy.orm import backref
 from market import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=True, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=True, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=True, unique=True)
+    budget = db.Column(db.Integer(), default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -6,6 +19,7 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name} {self.id}'
