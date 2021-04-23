14ab72f2dc1790e997f53e3d2d46cb863ba27b63
Manish Verma <vermam318@gmail.com>
2021-04-23 23:09:47 +0530

    register form class added

---
+++ flaskMarket/market/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.fields.simple import SubmitField
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(label='username')
+    email_address = StringField(label='email')
+    password1 = PasswordField(label='password1')
+    password2 = PasswordField(label='password2')
+    submit = SubmitField(label='submit')
